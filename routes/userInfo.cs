e63931fff0e0bab76c662af1b9f13aeba406e79e
gowtham kishore <gowthamkishore3@gmail.com>
2024-04-14 10:10:25 -0700

    Adding changes

--- routes/userInfo.cs
+++ routes/userInfo.cs
@@ -27,6 +27,7 @@ public class UserInformationRouter
             _logger.LogInformation("Processing a request to create or get a new user");
             string? deviceID = req.Query["deviceID"];
             string? name = req.Query["name"];
+            string? profilePic = req.Query["profilePicture"] ?? "1";
 
             if (string.IsNullOrEmpty(deviceID) || string.IsNullOrEmpty(name))
             {
@@ -35,7 +36,7 @@ public class UserInformationRouter
                 return new BadRequestObjectResult(errorJsonResponse);
             }
 
-            string userID = await _userInfoService.GetOrCreateUser(deviceID, name);
+            string userID = await _userInfoService.GetOrCreateUser(deviceID, name, profilePic);
 
             return new ContentResult
             {
@@ -70,9 +71,78 @@ public class UserInformationRouter
 
             string users = await _userInfoService.GetAllUsers(deviceID);
 
+            // Construct the JSON object string with the "users" key
+            string jsonObject = $"{{ \"users\": {users} }}";
+
             return new ContentResult
             {
-                Content = users,
+                Content = jsonObject,
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error processing the request");
+            return new BadRequestObjectResult("Error: Invalid request.");
+        }
+    }
+
+
+    [Function("PrimaryUser")]
+    public async Task<IActionResult> MakeUserPrimary(
+        [HttpTrigger(AuthorizationLevel.Function, "put", Route = "user/{userID}/primaryUser")] HttpRequestData req, string userID)
+    {
+        try
+        {
+            _logger.LogInformation("Processing a request to all new users");
+            string? deviceID = req.Query["deviceID"];
+
+            if (string.IsNullOrEmpty(deviceID) || string.IsNullOrEmpty(userID))
+            {
+                var errorResponse = new { message = "Error: deviceID/UserId is required." };
+                string errorJsonResponse = JsonConvert.SerializeObject(errorResponse);
+                return new BadRequestObjectResult(errorJsonResponse);
+            }
+
+            await _userInfoService.UpdateUserAsPrimary(deviceID, userID);
+            return new ContentResult
+            {
+                Content = "Updated Primary User Successfully...",
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error processing the request");
+            return new BadRequestObjectResult("Error: Invalid request.");
+        }
+    }
+
+    [Function("updateProfilePicture")]
+    public async Task<IActionResult> UpdateProfilePicture(
+        [HttpTrigger(AuthorizationLevel.Function, "put", Route = "user/{userID}/updateProfilePicture")] HttpRequestData req, string userID)
+    {
+        try
+        {
+            _logger.LogInformation("Processing a request to updateProfilePicture for userID");
+            string? profilePicture = req.Query["profilePicture"] ?? "1";
+
+            if (string.IsNullOrEmpty(profilePicture) || string.IsNullOrEmpty(userID))
+            {
+                var errorResponse = new { message = "Error: userID/profilePic is required." };
+                string errorJsonResponse = JsonConvert.SerializeObject(errorResponse);
+                return new BadRequestObjectResult(errorJsonResponse);
+            }
+
+            int profilePic = int.Parse(profilePicture);
+
+            await _userInfoService.UpdateProfilePicture(userID, profilePic);
+
+            return new ContentResult
+            {
+                Content = "Updated Profile Picture Successfully..",
                 ContentType = "application/json",
                 StatusCode = 200
             };
--- services/userInfo.cs
+++ services/userInfo.cs
@@ -16,7 +16,7 @@ public class userInformationServices
         dbConnection.OpenConnection();
     }
 
-    public async Task<string> GetOrCreateUser(string deviceID, string name)
+    public async Task<string> GetOrCreateUser(string deviceID, string name, string profilePic)
     {
 
         JObject userObject = await IsExistingUserBasedOnDeviceIDAndName(deviceID, name);
@@ -26,9 +26,17 @@ public class userInformationServices
         }
         else
         {
-            userObject = await CreateUser(deviceID, name);
+            int profileDp = int.Parse(profilePic);
+            await removeOtherPrimaryUser(deviceID);
+            userObject = await CreateUser(deviceID, name, profileDp);
             return userObject["UserID"]?.ToString() ?? ""; // Convert JToken to string, or return empty string if null
         }
+    }
+
+    public async Task removeOtherPrimaryUser(string deviceID)
+    {
+        string query = $"UPDATE Playerinfo SET IsPrimaryUser = 0 WHERE DeviceID = '{deviceID}'";
+        await dbConnection.ExecuteQueryAsync(query);
     }
 
 
@@ -54,7 +62,7 @@ public class userInformationServices
     }
 
 
-    private async Task<JObject> CreateUser(string deviceID, string name)
+    private async Task<JObject> CreateUser(string deviceID, string name, int profilePic)
     {
         JObject userObject = new JObject();
         string UserID = deviceID + "_" + name;
@@ -64,8 +72,9 @@ public class userInformationServices
         userObject["IsSoundEnabled"] = 1;
         userObject["IsMusicEnabled"] = 1;
         userObject["Age"] = 9;
-        string query = $"INSERT INTO Playerinfo (UserID, DeviceID, Name, IsSoundEnabled, IsMusicEnabled, Age) " +
-               $"VALUES ('{UserID}', '{deviceID}', '{userObject["Name"]}', '{userObject["IsSoundEnabled"]}', '{userObject["IsMusicEnabled"]}', '{userObject["Age"]}')";
+        userObject["ProfilePicture"] = profilePic != 0 ? profilePic : 1;
+        string query = $"INSERT INTO Playerinfo (UserID, DeviceID, Name, IsSoundEnabled, IsMusicEnabled, Age, ProfilePicture, IsPrimaryUser) " +
+               $"VALUES ('{UserID}', '{deviceID}', '{userObject["Name"]}', '{userObject["IsSoundEnabled"]}', '{userObject["IsMusicEnabled"]}', '{userObject["Age"]}', '{userObject["ProfilePicture"]}', 1)";
         await dbConnection.ExecuteQueryAsync(query);
         return userObject;
     }
@@ -152,6 +161,32 @@ public class userInformationServices
         }
         int soundEnabled = sound ? 1 : 0;
         string query = $"UPDATE Playerinfo SET IsSoundEnabled = '{soundEnabled}' WHERE UserID = '{userID}'";
+        await dbConnection.ExecuteQueryAsync(query);
+    }
+
+    public async Task UpdateProfilePicture(string userID, int profilePic)
+    {
+        JObject userObject = await IsExistingUserBasedOnUserID(userID);
+
+        if (!(userObject != null && userObject.HasValues))
+        {
+            throw new Exception("Error: Invalid user");
+        }
+
+        string query = $"UPDATE Playerinfo SET ProfilePicture = '{profilePic}' WHERE UserID = '{userID}'";
+        await dbConnection.ExecuteQueryAsync(query);
+    }
+
+    public async Task UpdateUserAsPrimary(string deviceID, string userID)
+    {
+
+        string query = $@"
+    UPDATE Playerinfo
+    SET IsPrimaryUser = CASE
+                            WHEN UserID = '{userID}' THEN 1
+                            ELSE 0
+                        END
+    WHERE DeviceID = '{deviceID}'";
         await dbConnection.ExecuteQueryAsync(query);
     }
 }
